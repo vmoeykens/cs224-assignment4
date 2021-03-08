22f98be37f04f1187e5a539e0dcecbd9f14796b0
vmoeykens <vincent@beta.team>
2021-03-07 20:16:02 -0500

    Begins implementing lecture class

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 import java.io.File;
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main { 
     public static void main(String[] args) 
@@ -25,4 +27,79 @@ public class Main {
         in.close();
         return lecturesFile;
     }
+}
+
+/**
+ * Class representing a classroom
+ */
+class Classroom {
+    int lastFin;
+
+/** 
+ * Class representing a lecture
+ */
+class Lecture {
+    String lectureName;
+    int startTime;
+    int endTime;
+
+    /**
+     * Lecture constructor
+     * @param lectureName a String that is used to name the lecture
+     * @param startTime the lecture start time as an integer
+     * @param endTime the lecture end time as an integer
+     */
+    public Lecture(String lectureName, int startTime, int endTime) {
+        this.lectureName = lectureName;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    /**
+     * Generate Lecture object from a string pulled from a file in the format:
+     * '(lectureName, startTime, endTime)'
+     * @param fileLine the string representing the file
+     */
+    public Lecture(String fileLine) {
+        ArrayList<String> lectureData = new ArrayList<>(Arrays.asList(fileLine.split(",[ ]*")));
+        this.lectureName = lectureData.get(0);
+        this.startTime = Integer.parseInt(lectureData.get(1));
+        this.endTime = Integer.parseInt(lectureData.get(2));
+    }
+
+    // Getters and setters
+    public String getLectureName() {
+        return this.lectureName;
+    }
+
+    public void setLectureName(String lectureName) {
+        this.lectureName = lectureName;
+    }
+
+    public int getStartTime() {
+        return this.startTime;
+    }
+
+    public void setStartTime(int startTime) {
+        this.startTime = startTime;
+    }
+
+    public int getEndTime() {
+        return this.endTime;
+    }
+
+    public void setEndTime(int endTime) {
+        this.endTime = endTime;
+    }
+
+    @Override
+    public String toString() {
+        return "(" +
+            getLectureName() + ", " +
+            getStartTime() + ", " +
+            getEndTime() +
+            ")";
+    }
+}
+
 }
