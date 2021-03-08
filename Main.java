7d4f49cb437f16ca7f3f8e7f5763b745a6c64116
vmoeykens <vincent@beta.team>
2021-03-07 19:56:11 -0500

    Adds file input validation

--- Main.java
+++ Main.java
@@ -1,8 +1,24 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class Main { 
-  
-    
     public static void main(String[] args) 
     { 
+        File lecturesFile = promptFile();
+    }
+
+    public static File promptFile() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a filename for input file.");
+        String fileName = in.nextLine();
+        File lecturesFile = new File(fileName);
         
+        while(!lecturesFile.exists()){
+            System.out.println("Invalid file name! Try again.");
+            fileName = in.nextLine();    
+            lecturesFile = new File(fileName);
+        }
+        in.close();
+        return lecturesFile;
     }
 }
